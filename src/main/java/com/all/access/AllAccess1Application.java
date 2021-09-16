77cbeb68ee21999e355c2a9e4d6479fdab0d6a5f
kirti shekhar pandey <kirtishekhar1997@gmail.com>
2021-09-17 00:28:20 +0530

    Commit by kirti

---
+++ src/main/java/com/all/access/AllAccess1Application.java
@@ -0,0 +1,13 @@
+package com.all.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllAccess1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllAccess1Application.class, args);
+	}
+
+}
